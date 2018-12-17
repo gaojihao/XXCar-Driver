267b6468d72aca5e5bfca9bf62386c26edc03725
lizhi <lizhi1026@126.com>
2018-12-17 20:44:54 +0800

    Reverse Geo Code

--- CarClient/CarClient/source/ViewController/MainViewController.m
+++ CarClient/CarClient/source/ViewController/MainViewController.m
@@ -18,10 +18,16 @@
 #import "UINavigationController+FDFullscreenPopGesture.h"
 
 
-@interface MainViewController ()<BMKMapViewDelegate,BMKLocationManagerDelegate>
+@interface MainViewController ()<BMKMapViewDelegate,BMKLocationManagerDelegate,BMKGeoCodeSearchDelegate>
 
 @property (nonatomic, strong) BMKMapView *mapView;
 @property (nonatomic, strong) BMKLocationManager *locationManager;
+@property (nonatomic) CLLocationCoordinate2D centerCoordinate;
+@property (nonatomic, strong)BMKPointAnnotation *point;
+@property (nonatomic, strong)BMKGeoCodeSearch *searcher;
+@property (nonatomic, strong) UIButton *locationBtn;
+
+@property (nonatomic, copy) NSString *startAddress;
 
 
 @end
@@ -79,6 +85,38 @@
     [self.mapView setCenterCoordinate:location.location.coordinate];
 }
 
+- (void)mapView:(BMKMapView *)mapView regionDidChangeAnimated:(BOOL)animated
+{
+    self.centerCoordinate =  mapView.centerCoordinate;
+    self.point.coordinate = self.centerCoordinate;
+    
+    BMKReverseGeoCodeSearchOption *reverseGeoCodeSearchOption = [[BMKReverseGeoCodeSearchOption alloc]init];
+    reverseGeoCodeSearchOption.location = mapView.centerCoordinate;
+    [self.searcher reverseGeoCode:reverseGeoCodeSearchOption];
+}
+
+- (void)onGetReverseGeoCodeResult:(BMKGeoCodeSearch *)searcher
+                           result:(BMKReverseGeoCodeSearchResult *)result
+                        errorCode:(BMKSearchErrorCode)error
+{
+    if (error == BMK_SEARCH_NO_ERROR) {
+        self.startAddress = result.sematicDescription;
+    } else {
+        NSLog(@"error======%d",error);
+    }
+}
+
+
+- (BMKAnnotationView *)mapView:(BMKMapView *)mapView viewForAnnotation:(id <BMKAnnotation>)annotation
+{
+    if ([annotation isKindOfClass:[BMKPointAnnotation class]]) {
+        BMKAnnotationView *newAnnotationView = [[BMKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"myAnnotation"];
+        newAnnotationView.image = [UIImage imageNamed:@"AppIcon"];
+        return newAnnotationView;
+    }
+    return nil;
+}
+
 - (BMKLocationManager *)locationManager
 {
     if (_locationManager == nil) {
@@ -88,12 +126,45 @@
     return _locationManager;
 }
 
+- (UIButton *)locationBtn
+{
+    if (_locationBtn == nil) {
+        _locationBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_locationBtn setImage:[UIImage imageNamed:@""] forState:UIControlStateNormal];
+        [_locationBtn setImage:[UIImage imageNamed:@""] forState:UIControlStateHighlighted];
+        [self.view addSubview:_locationBtn];
+    }
+    
+    return _locationBtn;
+}
+
+- (BMKPointAnnotation *)point
+{
+    if (_point == nil) {
+        _point = [[BMKPointAnnotation alloc] init];
+        
+        [self.mapView addAnnotation:_point];
+    }
+    
+    return _point;
+}
+
+- (BMKGeoCodeSearch *)searcher
+{
+    if (_searcher == nil) {
+        _searcher =[[BMKGeoCodeSearch alloc]init];
+        _searcher.delegate = self;
+    }
+    
+    return _searcher;
+}
+
 
 - (BMKMapView *)mapView
 {
     if (!_mapView) {
         _mapView = [[BMKMapView alloc] initWithFrame:self.view.bounds];
-        _mapView.userTrackingMode = BMKUserTrackingModeFollow;
+        _mapView.userTrackingMode = BMKUserTrackingModeNone;
         _mapView.delegate = self;
         _mapView.zoomEnabled = YES;
         _mapView.zoomEnabledWithTap = YES;
@@ -101,9 +172,6 @@
         _mapView.scrollEnabled = YES;
         _mapView.mapType = BMKMapTypeStandard;
         _mapView.zoomLevel = 18;
-        _mapView.baseIndoorMapEnabled  = YES;
-        _mapView.showIndoorMapPoi = YES;
-        _mapView.compassPosition = CGPointMake(15, 15);
         
         [self.view addSubview:_mapView];
     }
