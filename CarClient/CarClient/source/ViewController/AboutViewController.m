d38b34ac5afa5dca5af984bd1436651b4dd6bf08
lizhi <lizhi1026@126.com>
2018-12-19 21:56:40 +0800

    add about vc

---
+++ CarClient/CarClient/source/ViewController/AboutViewController.m
@@ -0,0 +1,21 @@
+//
+//  AboutViewController.m
+//  CarClient
+//
+//  Created by 栗志 on 2018/12/19.
+//  Copyright © 2018年 lizhi.1026.com. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
