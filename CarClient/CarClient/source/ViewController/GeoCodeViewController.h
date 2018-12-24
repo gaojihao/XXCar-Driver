7fe35c01c17f1965c1f1b3f7bb943a6b277b810b
lizhi <lizhi1026@126.com>
2018-12-24 22:25:41 +0800

    add geocode vc

---
+++ CarClient/CarClient/source/ViewController/GeoCodeViewController.h
@@ -0,0 +1,17 @@
+//
+//  GeoCodeViewController.h
+//  CarClient
+//
+//  Created by 栗志 on 2018/12/24.
+//  Copyright © 2018年 lizhi.1026.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GeoCodeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CarClient/CarClient/source/ViewController/GeoCodeViewController.m
@@ -0,0 +1,22 @@
+//
+//  GeoCodeViewController.m
+//  CarClient
+//
+//  Created by 栗志 on 2018/12/24.
+//  Copyright © 2018年 lizhi.1026.com. All rights reserved.
+//
+
+#import "GeoCodeViewController.h"
+
+@interface GeoCodeViewController ()
+
+@end
+
+@implementation GeoCodeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
