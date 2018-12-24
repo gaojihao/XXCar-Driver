43bc1eb2b99e55805cc91d3e3ef4fc8dffe2b412
lizhi <lizhi1026@126.com>
2018-12-24 22:24:05 +0800

    add guide vc

---
+++ CarClient/CarClient/source/ViewController/GuideViewController.h
@@ -0,0 +1,17 @@
+//
+//  GuideViewController.h
+//  CarClient
+//
+//  Created by 栗志 on 2018/12/24.
+//  Copyright © 2018年 lizhi.1026.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GuideViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CarClient/CarClient/source/ViewController/GuideViewController.m
@@ -0,0 +1,23 @@
+//
+//  GuideViewController.m
+//  CarClient
+//
+//  Created by 栗志 on 2018/12/24.
+//  Copyright © 2018年 lizhi.1026.com. All rights reserved.
+//
+
+#import "GuideViewController.h"
+
+@interface GuideViewController ()
+
+@end
+
+@implementation GuideViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
